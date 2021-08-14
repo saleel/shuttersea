f0e5aee40286aba2b74c5be255c6b3a9284fcd69
Saleel Salam <saleel97@gmail.com>
2021-08-14 13:10:00 +0400

    Add Esc key modal close

--- components/photo-modal.js
+++ components/photo-modal.js
@@ -4,11 +4,26 @@ import PhotoViewer from './photo-viewer';
 export default function PhotoGrid(props) {
   const { photo, onClose } = props;
 
+  React.useEffect(() => {
+    document.addEventListener('keydown', () => {
+      const evt = window.event;
+      let isEscape = false;
+      if ('key' in evt) {
+        isEscape = (evt.key === 'Escape' || evt.key === 'Esc');
+      } else {
+        isEscape = (evt.keyCode === 27);
+      }
+      if (isEscape) {
+        onClose();
+      }
+    });
+  });
+
   return (
     <div className="modal is-active is-clipped">
       <div className="modal-background" />
 
-      <div className="modal-content photo-container">
+      <div className="photo-modal">
         <PhotoViewer photo={photo} />
       </div>
 
